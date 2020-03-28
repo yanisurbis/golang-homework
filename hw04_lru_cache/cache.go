f82986bcaedf32d75a03614ca386316cc7c5941f
Yanis <yanis.urbis@gmail.com>
2020-03-28 16:19:40 +0300

    Add solution

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -2,21 +2,64 @@ package hw04_lru_cache //nolint:golint,stylecheck
 
 type Key string
 
-type Cache interface {
-	// Place your code here
-}
-
-type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
-}
-
 type cacheItem struct {
 	// Place your code here
 }
 
-func NewCache(capacity int) Cache {
-	return &lruCache{}
+type Cache interface {
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
+}
+
+type lruCache struct {
+	Size int
+	Queue List
+	Elements map[Key]*listItem
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	element, found := cache.Elements[key]
+	if found {
+		cache.Queue.Remove(element)
+		cache.Elements[key] = cache.Queue.PushFront(value)
+		return true
+	} else {
+		if cache.Queue.Len() == cache.Size {
+			// delete element
+			elmToDelete := cache.Queue.Back()
+			cache.Queue.Remove(elmToDelete)
+			for key, elm := range cache.Elements {
+				if elm == elmToDelete {
+					delete(cache.Elements, key)
+				}
+			}
+		}
+		elm := cache.Queue.PushFront(value)
+		cache.Elements[key] = elm
+		return false
+	}
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	element, found := cache.Elements[key]
+	if found {
+		cache.Queue.MoveToFront(element)
+		return element.Value, true
+	} else {
+		return nil, false
+	}
+}
+
+func (cache *lruCache) Clear() {
+	cache.Queue = NewList()
+	cache.Elements = make(map[Key]*listItem, cache.Size)
+}
+
+func NewCache(size int) Cache {
+	return &lruCache{
+		Size:     size,
+		Queue:    NewList(),
+		Elements: make(map[Key]*listItem, size),
+	}
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,17 +1,140 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
-type List interface {
-	// Place your code here
-}
-
 type listItem struct {
-	// Place your code here
+	Value interface{}
+	Next *listItem
+	Prev *listItem
 }
 
 type list struct {
-	// Place your code here
+	Head   *listItem
+	Tail   *listItem
+	Length int
+}
+
+type List interface {
+	Len() int
+	Front() *listItem
+	Back() *listItem
+	PushFront(v interface{}) *listItem
+	PushBack(v interface{}) *listItem
+	Remove(i *listItem)
+	MoveToFront(i *listItem)
+}
+
+func (l *list) Len() int {
+	return l.Length
+}
+
+func (l *list) Front() *listItem {
+	return l.Head
+}
+
+func (l *list) Back() *listItem {
+	return l.Tail
+}
+
+func (l *list) PushFront(v interface{}) *listItem {
+	if l.Length == 0 {
+		newElm := &listItem{
+			Value: v,
+			Next:  nil,
+			Prev:  nil,
+		}
+		l.Head = newElm
+		l.Tail = newElm
+		l.Length = 1
+
+		return newElm
+	} else {
+		head := l.Head
+		newElm := &listItem{
+			Value: v,
+			Next:  head,
+			Prev:  nil,
+		}
+		head.Prev = newElm
+		l.Head = newElm
+		l.Length += 1
+
+		return newElm
+	}
+}
+
+func (l *list) PushBack(v interface{}) *listItem {
+	if l.Length == 0 {
+		newElm := &listItem{
+			Value: v,
+			Next:  nil,
+			Prev:  nil,
+		}
+		l.Head = newElm
+		l.Tail = newElm
+		l.Length = 1
+
+		return newElm
+	} else {
+		tail := l.Tail
+		newElm := &listItem{
+			Value: v,
+			Next:  nil,
+			Prev:  tail,
+		}
+		tail.Next = newElm
+		l.Tail = newElm
+		l.Length += 1
+
+		return newElm
+	}
+}
+
+func (l *list) Remove(i *listItem) {
+	// QUESTION: how to make sure the list contains this particular item?
+	if i == nil || l.Length == 0 {
+		return
+	}
+
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+		l.Length -= 1
+		return
+	}
+
+	if i.Prev == nil && i.Next != nil {
+		l.Head = i.Next
+		l.Head.Prev = nil
+		l.Length -= 1
+		return
+	}
+
+	if i.Prev != nil && i.Next == nil {
+		l.Tail = i.Prev
+		l.Tail.Next = nil
+		l.Length -= 1
+		return
+	}
+
+	if i.Prev == nil && i.Next == nil {
+		l.Head = nil
+		l.Tail = nil
+		l.Length = 0
+		return
+	}
+}
+
+func (l *list) MoveToFront(i *listItem) {
+	if i == nil {
+		return
+	}
+	l.Remove(i)
+	l.PushFront(i.Value)
 }
 
 func NewList() List {
-	return &list{}
+	return &list{
+		Head:   nil,
+		Tail:   nil,
+		Length: 0,
+	}
 }
