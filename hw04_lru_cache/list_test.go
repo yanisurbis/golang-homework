d0ab36897808f042330aab93eb9b143181b30846
Yanis <yanis.urbis@gmail.com>
2020-03-28 16:14:28 +0300

    Fix test

--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -23,7 +23,7 @@ func TestList(t *testing.T) {
 		l.PushBack(30)  // [10, 20, 30]
 		require.Equal(t, l.Len(), 3)
 
-		middle := l.Back().Next // 20
+		middle := l.Back().Prev // 20
 		l.Remove(middle)        // [10, 30]
 		require.Equal(t, l.Len(), 2)
 
@@ -43,7 +43,7 @@ func TestList(t *testing.T) {
 		l.MoveToFront(l.Back())  // [70, 80, 60, 40, 10, 30, 50]
 
 		elems := make([]int, 0, l.Len())
-		for i := l.Back(); i != nil; i = i.Next {
+		for i := l.Back(); i != nil; i = i.Prev {
 			elems = append(elems, i.Value.(int))
 		}
 		require.Equal(t, []int{50, 30, 10, 40, 60, 80, 70}, elems)
